ff918250b697eabdef8d92e972658d024b4dd2a2
Yang Liu <byliuyang11@gmail.com>
2017-06-18 05:06:03 -0400

    Add datetime to comments

--- extension/scripts/app/sidebar.component.js
+++ extension/scripts/app/sidebar.component.js
@@ -4,8 +4,22 @@ function SideBarComponent() {
         '<div class="weigh-in-sidebar-header">' +
             '<img class="icon" src="https://s3.us-east-2.amazonaws.com/weigh-in/icon256.png"/>' +
                 '<div class="title"><span>2</span> Comments</div>' +
-        '</div>';
-    this.width = 300;
+        '</div>' +
+        '<ul id="weigh-in-sidebar-comment-list">' +
+            '<li>' +
+                '<div><span class="weigh-in-username">3NYTimes</span>: Great article, checkout this other article by Maurine Dowd: https://www.nytimes.com/2017/05/27/opinion/sunday/trumps-hand-to-hand-combat.html</div>' +
+                '<div class="weigh-in-datetime">1 second ago</div>' +
+            '</li>' +
+            '<li>' +
+                '<div><span class="weigh-in-username">Feminist123</span>: This article only skims the surface of issues sur-rounding sexism in the United States.</div>' +
+                '<div class="weigh-in-datetime">10 minutes ago</div>' +
+            '</li>' +
+            '<li>' +
+                '<div><span class="weigh-in-username">US</span>: This is cool!</div>' +
+                '<div class="weigh-in-datetime">June 5th</div>' +
+            '</li>' +
+        '</ul>';
+    this.width = 350;
     var self = this;
     this.show = function () {
         self.page.style.width = (document.documentElement.offsetWidth - this.width) + 'px';
--- extension/scripts/script.js
+++ extension/scripts/script.js
@@ -2,6 +2,8 @@ window.onload = function () {
     var toggleSwitch = document.querySelector('#switch-toggle');
     isPlugInEnabled(function (enabled) {
             toggleSwitch.checked = enabled;
+        enablePlugIn(enabled);
+
     });
     toggleSwitch.addEventListener('change', function () {
         if(toggleSwitch.checked)
@@ -12,6 +14,8 @@ window.onload = function () {
 };
 
 function enablePlugIn(enabled) {
+    if(enabled) chrome.browserAction.setIcon({path: 'images/icon128.png'});
+    else chrome.browserAction.setIcon({path: 'images/icon128-grey.png'});
     chrome.storage.sync.set({'weighIn-enabled': enabled});
 }
 
