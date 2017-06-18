d89595b68adc73ad5dd60e390cbb0989a2862665
Harry Sadoyan <harrysadoyan@gmail.com>
2017-06-18 10:40:13 -0400

    temp

--- weighin_rails/config/routes.rb
+++ weighin_rails/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
+  get '/messages' => 'message#show'
+  post '/messages' => 'message#update'
 end
